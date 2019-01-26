c1f124c50edba21e589da00bb83b7f2a5ebd4b8a
stopachka <stepan.p@gmail.com>
2019-01-26 13:04:46 -0800

    sm

--- lib/chat_web/channels/room_channel.ex
+++ lib/chat_web/channels/room_channel.ex
@@ -18,12 +18,4 @@ defmodule ChatWeb.RoomChannel do
 
     {:noreply, socket}
   end
-
-  # def handle_info(:after_join, socket) do
-  #   Presence.track(socket, socket.assigns.user, %{
-  #     online_at: :os.system_time(:milli_seconds)
-  #   })
-  #   push socket, "presence_state", Presence.list(socket)
-  #   {:noreply, socket}
-  # end
 end
