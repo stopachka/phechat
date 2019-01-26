91d979264d371b2bf2e9954621c0d4feebd68dd2
stopachka <stepan.p@gmail.com>
2019-01-26 13:06:00 -0800

    sm

--- lib/chat_web/channels/room_channel.ex
+++ lib/chat_web/channels/room_channel.ex
@@ -3,14 +3,9 @@ defmodule ChatWeb.RoomChannel do
   require Logger
 
   def join("room:lobby", msg, socket) do
-    Process.flag(:trap_exit, true)
     send(self, {:after_join, msg})
 
     {:ok socket}
-  end
-
-  def join("rooms:" <> _private_subtopic, msg, _socket) do
-    {:error, %{reason: "unauthorized"}}
   end
 
   def handle_info({:after_join, msg}, socket) do
