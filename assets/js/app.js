9c163483cd23f14fbb4adf3c13fc904984ee7ac5
stopachka <stepan.p@gmail.com>
2019-01-26 13:24:41 -0800

    sm

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,77 @@
-import "phoenix_html"
-import {Socket, Presence} from "phoenix"
-import css from "../css/app.css"
-import React from 'react';
-import ReactDOM from 'react-dom';
+import "phoenix_html";
+import { Socket, Presence } from "phoenix";
+import css from "../css/app.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import _ from "lodash";
+
+function makeNickname() {
+  const firstParts = [
+    "beautiful",
+    "laughing",
+    "strong",
+    "smart",
+    "handsome",
+    "nice",
+    "gentle",
+    "kindly"
+  ];
+  const secondParts = [
+    "zebra",
+    "giraffe",
+    "lion",
+    "anaconda",
+    "tiger",
+    "dragon"
+  ];
+  return `${_.sample(firstParts)}-${_.sample(secondParts)}`;
+}
 
 class App extends React.Component {
+  state = {
+    name: makeNickname(),
+    message: ""
+  };
   componentDidMount() {
-    const user = "Stopa";
-    const socket = new Socket("/socket", {params: {user}});
-    socket.connect()
+    this._msgInput && this._msgInput.focus();
+    // const user = "Stopa";
+    // const socket = new Socket("/socket", { params: { user } });
+    // socket.connect();
+  }
+  _sendMessage() {
+    alert('send!' + this.state.message);
   }
   render() {
-    return <div>hello world</div>
+    return (
+      <div className="App">
+        <div className="chat-container">
+          Type something...messages will show up here :)
+        </div>
+        <div className="chat-actions">
+          <h3 className="chat-name">{this.state.name}</h3>
+          <input
+            ref={x => {
+              this._msgInput = x;
+            }}
+            type="text"
+            value={this.state.message}
+            onChange={e => {
+              this.setState({ message: e.target.value });
+            }}
+            onKeyDown={e => {
+              debugger
+              switch (e.key) {
+                case "Enter":
+                  this._sendMessage();
+                default:
+                  break;
+              }
+            }}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root-container')
-);
+ReactDOM.render(<App />, document.getElementById("root-container"));
--- lib/chat_web/channels/room_channel.ex
+++ lib/chat_web/channels/room_channel.ex
@@ -1,15 +1,14 @@
 defmodule ChatWeb.RoomChannel do
   use Phoenix.Channel
-  require Logger
 
   def join("room:lobby", msg, socket) do
-    send(self, {:after_join, msg})
+    # send(self, {:after_join, msg})
 
-    {:ok socket}
+    {:ok, socket}
   end
 
   def handle_info({:after_join, msg}, socket) do
-    broadcast! socket, "user:entered", %{user: msg["user"]}
+    # broadcast! socket, "user:entered", %{user: msg["user"]}
 
     {:noreply, socket}
   end
