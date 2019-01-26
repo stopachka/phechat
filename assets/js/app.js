b26c292c248def1a710196fbe62364d199d6f324
stopachka <stepan.p@gmail.com>
2019-01-26 12:52:35 -0800

    sm

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,21 @@
-import css from "../css/app.css"
 import "phoenix_html"
 import {Socket, Presence} from "phoenix"
+import css from "../css/app.css"
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const user = document.getElementById("user").innerText;
-const socket = new Socket("/socket", {params: {user}});
-socket.connect()
+class App extends React.Component {
+  componentDidMount() {
+    const user = "Stopa";
+    const socket = new Socket("/socket", {params: {user}});
+    socket.connect()
+  }
+  render() {
+    return <div>hello world</div>
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root-container')
+);
