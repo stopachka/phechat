37e6c9fea5609508f2269312d890e07586003b24
stopachka <stepan.p@gmail.com>
2019-01-26 14:11:58 -0800

    sm

--- lib/chat_web/endpoint.ex
+++ lib/chat_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule ChatWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :chat
 
   socket "/socket", ChatWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
